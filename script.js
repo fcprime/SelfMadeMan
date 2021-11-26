f271d5efd88f4c63151c04f7a399fffc90d5b03e
Serhii Norov <Serhiinorov110@gmail.com>
2021-11-26 22:02:04 +0100

    new file

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log('Hello World!');
+alert ("Hello my name is Serhii");
+console.log('My firstname - Norov');
+
+
+let title ;
+let screens ;
+let ScreenPrice ;
+let rollback ;
+let fullPrice ;
+let adaptive ;
+
